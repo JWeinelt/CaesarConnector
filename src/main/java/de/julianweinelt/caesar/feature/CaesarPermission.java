cab348cc400b18d901e4bd170da554855b2e1792
Julian Weinelt <julian.weinelt@outlook.de>
2025-07-14 15:14:56 +0200

    fix: Update permissions

--- src/main/java/de/julianweinelt/caesar/feature/CaesarPermission.java
+++ src/main/java/de/julianweinelt/caesar/feature/CaesarPermission.java
@@ -6,13 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CaesarPermission {
-    public static final Permission BAN_COMMAND = new Permission("caesar.ban");
-    public static final Permission UNBAN_COMMAND = new Permission("caesar.unban");
-    public static final Permission MUTE_COMMAND = new Permission("caesar.mute");
-    public static final Permission UNMUTE_COMMAND = new Permission("caesar.unmute");
-    public static final Permission KICK_COMMAND = new Permission("caesar.kick");
-    public static final Permission WARN_COMMAND = new Permission("caesar.warn");
-    public static final Permission REPORT_COMMAND = new Permission("caesar.report");
+    public static final Permission BAN_COMMAND = new Permission("caesar.command.ban");
+    public static final Permission UNBAN_COMMAND = new Permission("caesar.command.unban");
+    public static final Permission MUTE_COMMAND = new Permission("caesar.command.mute");
+    public static final Permission UNMUTE_COMMAND = new Permission("caesar.command.unmute");
+    public static final Permission KICK_COMMAND = new Permission("caesar.command.kick");
+    public static final Permission WARN_COMMAND = new Permission("caesar.command.warn");
+    public static final Permission REPORT_COMMAND = new Permission("caesar.command.report");
+    public static final Permission NOTIFY = new Permission("caesar.notify");
 
     public static List<Permission> values() {
         List<Permission> p = new ArrayList<>();
